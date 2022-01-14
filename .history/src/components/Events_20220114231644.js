7a1a7708bdbb4e02c3432a2494cefa9be29641c3
sie-fufu <ca.fukuda@gmail.com>
2022-01-14 23:27:31 +0900

    refactor components

---
+++ .history/src/components/Events_20220114231644.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Events = () => {
+  retrun(
+    <>
+      <h4>イベント一覧</h4>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>ボディ</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.map((event, index) => (
+            <Event key={index} event={event} dispatch={dispatch} />
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
